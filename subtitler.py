5beccc3b914bfde29cc45da82637577549c19aa5
Gabriel Montagné Láscaris-Comneno <gabriel@rojored.com>
2016-02-05 19:28:34 +0000

    frame handler stub.

--- subtitler.py
+++ subtitler.py
@@ -1,7 +1,17 @@
+import bpy
+from bpy.app.handlers import persistent
 
+bl_info = { "name": "subtitle text render", "category": "Object" }
+
+@persistent
+def frame_pre(scene):
+    print("Frame Change", scene.frame_current)
 
 def register():
     print('registering')
+
+    bpy.app.handlers.frame_change_pre.clear()
+    bpy.app.handlers.frame_change_pre.append(frame_pre)
 
 
 def unregister():
